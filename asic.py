e48ae1d34fee39920d434f6e5972935f5da1d47d
thevoyagertwo <thevoyagertwo@protonmail.com>
2021-11-15 15:34:30 +0000

    Added delay handling through L49: months_to_roi = delay[i] # reset months to roi

--- asic.py
+++ asic.py
@@ -6,7 +6,7 @@ Thashrate = [78,84,78,110] # Thash
 miner_price = [9800,12848,9800,17000]# $
 wattage = [3400,3150,3400,3250] # watts
 elec_cost = [0.063,0.067,0.00000000,0.045] # $ per kwh
-delay = [0] # months before online
+delay = [0,0,0,4] # months before online
 
 difficulty_increase = [1.05,1.10,1.15] # %/100 a year + 1
 
@@ -42,15 +42,15 @@ def roi(difficulty,difficulty_increase_month,miner_price_btc,hashrate,wattage,el
 
     for i in range(0,len(miner_price)):
         print("Thash=",hashrate[i]/1000000000000," price=",round(miner_price_btc[i],3))
-        incdifficulty = difficulty
         for j in range(0,len(difficulty_increase_month)):
+            incdifficulty = difficulty # reset btc diff
             btc_mined = 0 # reset btc mined
-            months_to_roi = 0 # reset months to roi
+            months_to_roi = delay[i] # reset months to roi
             while (btc_mined < miner_price_btc[i] and months_to_roi < 24):
                 btc_mined = btc_mined + btc_pm(incdifficulty, hashrate[i], wattage[i], elec_cost[i], btc_price, uptime)
 
-                months_to_roi = months_to_roi +1
-                incdifficulty = incdifficulty*difficulty_increase_month[j]
+                months_to_roi = months_to_roi + 1
+                incdifficulty = difficulty*difficulty_increase_month[j]**months_to_roi
             print("month=",months_to_roi," BTC=",round(btc_mined,5)," diff% = ", round((difficulty_increase_month[j]**12-1)*100),"%" )
 
 
@@ -58,3 +58,21 @@ def roi(difficulty,difficulty_increase_month,miner_price_btc,hashrate,wattage,el
 
 print(btc_pm(difficulty,hashrate[0],wattage[0],elec_cost[0],btc_price,uptime))
 roi(difficulty,difficulty_increase_month,miner_price_btc,hashrate,wattage,elec_cost,btc_price,uptime)
+
+## example output
+# Thash= 78.0  price= 0.144
+# month= 13  BTC= 0.14563  diff% =  5 %
+# month= 15  BTC= 0.15182  diff% =  10 %
+# month= 18  BTC= 0.14895  diff% =  15 %
+# Thash= 84.0  price= 0.189
+# month= 16  BTC= 0.19496  diff% =  5 %
+# month= 18  BTC= 0.19429  diff% =  10 %
+# month= 23  BTC= 0.19356  diff% =  15 %
+# Thash= 78.0  price= 0.144
+# month= 11  BTC= 0.14886  diff% =  5 %
+# month= 12  BTC= 0.15175  diff% =  10 %
+# month= 13  BTC= 0.14567  diff% =  15 %
+# Thash= 110.0  price= 0.25
+# month= 15  BTC= 0.26066  diff% =  5 %
+# month= 16  BTC= 0.25153  diff% =  10 %
+# month= 20  BTC= 0.25849  diff% =  15 %
